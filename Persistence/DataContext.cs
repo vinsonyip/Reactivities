9dd231bdbc7a8d0a20cc86d747a2ccfe383139fa
Vinson Yip <yipvinson@gmail.com>
2023-05-26 23:38:15 +0800

    End of section 2

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Domain;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Activity> Activities { get; set; }
+    }
+}
