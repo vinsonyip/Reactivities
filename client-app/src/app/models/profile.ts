0017fa6aa750d09aaff9a6e1211d236bb2001b7c
vinsonyip <yipvinson@gmail.com>
2023-10-16 00:54:34 +0800

    End of section 17

--- client-app/src/app/models/profile.ts
+++ client-app/src/app/models/profile.ts
@@ -1,16 +1,29 @@
 import { User } from "./user";
 
-export interface Profile{
+export interface IProfile{
     username: string;
     displayName: string;
     image?: string;
     bio?: string;
+    photos?: Photo[];
 }
 
-export class Profile implements Profile {
+export class Profile implements IProfile {
+    [x: string]: any;
     constructor(user: User){
         this.username = user.username;
         this.displayName = user.displayName;
         this.image = user.image;
     }
+    username: string;
+    displayName: string;
+    image?: string;
+    bio?: string;
+    photos?: Photo[];
+}
+
+export interface Photo {
+    id: string;
+    url: string;
+    isMain: boolean;
 }
